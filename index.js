8bc9032ba330300ae13b4df5cd6a8a5b009533be
Konstantin Onufrijev <koonuf@gmail.com>
2014-10-28 13:08:39 +0200

    module and tests initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+﻿function validateType(obj, type, error) {
+    if (typeof obj !== type) {
+        throw error;
+    }
+}
+
+function validateWrap(resultMap, name, func) {
+    validateType(name, 'string', 'Name should be a strng');
+    validateType(func, 'function', 'Func should be a function');
+    
+    if (resultMap.hasOwnProperty(name)) {
+        throw 'Name "' + name + '" already defined';
+    }
+}
+
+function checkAllDone(numOfPendingOps, onAllDoneFunc, resultMap, cancelled) {
+    if (!cancelled && numOfPendingOps === 0 && typeof onAllDoneFunc !== 'undefined') {
+        onAllDoneFunc(resultMap);
+    }
+}
+
+function ParallelGroup() {
+    var resultMap = {}, 
+        numOfPendingOps = 0, 
+        onAllDoneFunc,
+        cancelled = false;
+    
+    this.wrap = function (name, context, func) {
+        
+        if (typeof context === 'function') {
+            func = context;
+            context = null;
+        }
+        
+        validateWrap(resultMap, name, func);
+        
+        resultMap[name] = undefined;
+        numOfPendingOps++;
+        context = typeof context === 'object' ? context : null;
+        
+        return function () {
+            var result;
+            try {
+                result = func.apply(context, arguments);
+            } catch (e) { 
+                result = e;
+            }
+            
+            resultMap[name] = result;
+            numOfPendingOps--;
+            checkAllDone(numOfPendingOps, onAllDoneFunc, resultMap, cancelled);
+        };
+    };
+    
+    this.onAllDone = function (func, context) {
+        validateType(func, 'function', 'Func should be a function');
+        if (typeof onAllDoneFunc !== 'undefined') {
+            throw 'All done callback already defined';
+        }
+        
+        onAllDoneFunc = typeof context === 'object' ? func.bind(context) : func;
+        
+        checkAllDone(numOfPendingOps, onAllDoneFunc, resultMap, cancelled);
+    };
+    
+    this.cancel = function () { cancelled = true; };
+    
+    this.isCancelled = function () { return cancelled; };
+}
+
+function construct() {
+    return new ParallelGroup();
+}
+
+module.exports = construct;
---
+++ test/tests.js
@@ -0,0 +1,102 @@
+﻿var assert = require('assert');
+var groupCreator = require('./../index.js');
+var fs = require('fs');
+var expect = require('expect.js');
+
+function fileReadResults(err, data) {
+    return err || data.toString().trim();
+}
+
+function assertResults(results, key, expectedValue) {
+    var actual = results[key];
+    expect(actual).to.eql(expectedValue);
+}
+
+describe('Opening files', function () {
+    it('should produce map with file contents', function (done) {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', fileReadResults));
+        fs.readFile('./test/test2.txt', group.wrap('file2', fileReadResults));
+        fs.readFile('./test/test3.txt', group.wrap('file3', fileReadResults));
+        
+        group.onAllDone(function (results) {
+            assertResults(results, 'file1', 'text 1');
+            assertResults(results, 'file2', 'text 2');
+            assertResults(results, 'file3', 'text 3');
+            
+            done();
+        });
+    });
+
+    it('should not call all done callback if cancelled', function (done) {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', function () {
+           group.cancel();
+           expect(group.isCancelled()).to.eql(true);
+            
+           setTimeout(done);
+        }));
+
+        group.onAllDone(function (results) {
+            expect().fail("called all done handler unexpectedly");
+        });
+    });
+
+    it('should map error in wrapped callback', function (done) {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', fileReadResults));
+        fs.readFile('./test/test2.txt', group.wrap('file2', function () { 
+            throw { erroCode: 5 };
+        }));
+        
+        group.onAllDone(function (results) {
+            assertResults(results, 'file1', 'text 1');
+            expect(results['file2']).to.eql({ erroCode: 5 });
+            
+            done();
+        });
+    });
+
+    it('should validate duplicate names', function () {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', fileReadResults));
+        
+        function testDuplicate() 
+        { 
+            fs.readFile('./test/test1.txt', group.wrap('file1', fileReadResults));
+        }
+
+        expect(testDuplicate).to.throwException(function (e)
+        {
+            expect(e).to.eql('Name "file1" already defined');
+        });
+    });
+    
+    it('should not allow double all done handler registration', function () {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', fileReadResults));
+        group.onAllDone(function () {});
+        
+        function testDuplicateAllDone() {
+            group.onAllDone(function () { });
+        }
+        
+        expect(testDuplicateAllDone).to.throwException(function (e) {
+            expect(e).to.eql('All done callback already defined');
+        });
+    });
+
+    it('should pass context to wrapped handler', function (done) {
+        var group = groupCreator();
+        
+        fs.readFile('./test/test1.txt', group.wrap('file1', { contextProp: 1 }, function () { 
+            expect(this).to.eql({ contextProp: 1 });
+            done();
+        }));
+    });
+});
